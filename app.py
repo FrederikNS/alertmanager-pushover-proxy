7863d977d6e37efe6cbbf861841067b22cfb1e58
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2023-07-25 17:43:46 +0200

    Switch to receiving alertmanager webhooks

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
 from flask import Flask, request
 import requests
+import logging
+import os
 
 app = Flask(__name__)
 
-POST_URL = "https://api.pushover.net/1/messages.json"
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
-@app.route('/1/messages.json', methods=['POST'])
-def bomb():
+POST_URL = "https://api.pushover.net/1/messages.json"
+USER_KEY = os.environ["PUSHOVER_USER_KEY"]
+TOKEN = os.environ["PUSHOVER_TOKEN"]
+
+@app.route('/', methods=['POST'])
+def webhook():
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        body = request.json
-        requests.post(POST_URL, json={
-            "token": body["token"],
-            "user": body["user"],
-            "message": body["message"],
-            "priority": body["priority"],
-            "sound": body["sound"],
-            "title": body["title"],
-            "ttl": 3600
-        })
-        return {"status": "OK"}
-    return {"status": "Error"}, 400
+    if (content_type != 'application/json'):
+        return {"status": "Error"}, 400
+
+    body = request.json
+    if body["status"] != "firing":
+        return {"status": "NOOP"}
+
+    app.logger.info(body)
+    requests.post(POST_URL, json={
+        "token": TOKEN,
+        "user": USER_KEY,
+        "message": str(body["groupLabels"]),
+        "priority": 0,
+        "title": body["groupKey"],
+        "ttl": 3600
+    })
+
+    return {"status": "OK"}
 
 @app.route('/healthz')
 def health():
